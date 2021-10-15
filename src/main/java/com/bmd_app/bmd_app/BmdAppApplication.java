992a35b7d5c4c3caadd536d9bd55c0d13af874c3
Batuhan Inan <inanbatuhan61@gmail.com>
2021-10-15 17:11:00 +0300

    initial commit

---
+++ src/main/java/com/bmd_app/bmd_app/BmdAppApplication.java
@@ -0,0 +1,13 @@
+package com.bmd_app.bmd_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmdAppApplication.class, args);
+	}
+
+}
