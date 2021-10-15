32c6d493ad4d792958ea0ee9757c733c997d9d9e
atilla525 <52794740+atilla525@users.noreply.github.com>
2021-10-15 21:31:43 +0300

    Update 1.01
    RequestCount

--- src/main/java/com/bmd_app/bmd_app/Controller/QueryController.java
+++ src/main/java/com/bmd_app/bmd_app/Controller/QueryController.java
@@ -1,12 +1,18 @@
 package com.bmd_app.bmd_app.Controller;
 
 import com.bmd_app.bmd_app.Entity.Client;
+import com.bmd_app.bmd_app.Entity.Request;
 import com.bmd_app.bmd_app.Repository.ClientRepository;
 import com.bmd_app.bmd_app.Repository.DeliveryRepository;
 import com.bmd_app.bmd_app.Repository.RequestRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 @Controller
 @RequestMapping(path="/api/query")
@@ -21,11 +27,24 @@ public class QueryController {
 	@Autowired
 	RequestRepository requestRepository;
 
-	@GetMapping(path="/count/request")
-	public @ResponseBody Iterable<Client> getRequestCount (){
-		// TODO: Parse JSON
+	@Autowired
+	ObjectMapper mapper;
 
-		return clientRepository.findAll();
+	@GetMapping(path="/count/request")
+	public @ResponseBody ObjectNode getRequestCount (@RequestBody Date startTime, @RequestBody Date endTime){
+		ObjectNode response = mapper.createObjectNode();
+		int count = 0;
+		ArrayList<Request> requests = (ArrayList<Request>) requestRepository.findAll();
+		for (Request request : requests){
+			if (request.getStartTime().after(startTime)){
+				if (endTime.after(request.getEndTime())){
+					count++;
+				}
+			}
+		}
+		response.put("status","success");
+		response.put("count",count);
+		return response;
 	}
 
 	@GetMapping(path="/count/stats")
